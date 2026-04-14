bb40a8c0f646753abf1385a0e854c843fbf66092
jbfrolich <jfrolich@calpoly.edu>
2026-04-14 14:48:51 -0700

    Answered comments for task 3 part 2

--- main.py
+++ main.py
@@ -11,13 +11,16 @@ message = welcome_message("jfrolich@calpoly.edu")
 print(message)
 
 
-def smallest(n: float, m: float) -> float:
-   if n < m:
-      return n  # For which calls below is this statement evaluated? n > m
+def function2(a: int, b: int, c: int) -> int:
+   if a > b and a > c:
+      return a - b  # when a is the largest
+   elif b > c:
+      return b + c  # when b>c and a is not the largest
    else:
-      return m
+      return 2 * c  # when c is greater than or equal to c and a is not the largest
 
 
-first = smallest(3, 2)  # What is the value of first? 2
-second = smallest(2, 2)  # The value is 2 but it is not reasonable because 2 = 2
+answer1 = function2(3, 2, 1)  # What is the value of answer1? 1
+answer2 = function2(2, 3, 1)  # What is the value of answer2? 4
+answer3 = function2(2, 1, 3)  # What is the value of answer3? 6
 print()
