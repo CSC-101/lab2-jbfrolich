c83722cd2363f1a91afbbe7ed2153395343582ba
jbfrolich <jfrolich@calpoly.edu>
2026-04-14 14:46:02 -0700

    Answered comments for task 3 part 1

--- main.py
+++ main.py
@@ -9,3 +9,15 @@ def welcome_message(name:str) -> str:
 
 message = welcome_message("jfrolich@calpoly.edu")
 print(message)
+
+
+def smallest(n: float, m: float) -> float:
+   if n < m:
+      return n  # For which calls below is this statement evaluated? n > m
+   else:
+      return m
+
+
+first = smallest(3, 2)  # What is the value of first? 2
+second = smallest(2, 2)  # The value is 2 but it is not reasonable because 2 = 2
+print()
